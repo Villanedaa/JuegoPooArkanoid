b2e596c23eabe9604a3910e800c2754f6abe2f2b
juanCamilo <juanc.cardonav@autonoma.edu.co>
2025-05-12 20:22:35 -0500

    hola

---
+++ test/juego.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author GEIDG
+ */
+public class juego {
+    public static void main(String[] args) {
+        System.out.println("hola");
+    }
+}
