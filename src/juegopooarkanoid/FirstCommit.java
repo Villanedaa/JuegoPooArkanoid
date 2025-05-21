bfa62aba1ab32c7c80128e01cf39a29e68c5201b
Sebastian <sebastianvillaneda202@gmail.com>
2025-05-20 23:10:18 -0500

    prueba

--- src/juegopooarkanoid/FirstCommit.java
+++ src/juegopooarkanoid/FirstCommit.java
@@ -2,13 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package juegopooarkanoid;
 
 /**
  *
- * @author GEIDG
+ * @author Sebastian
  */
-public class juego {
+public class FirstCommit {
     public static void main(String[] args) {
-        System.out.println("hola");
+        System.out.println("Commit de prueba");
     }
+    
 }
