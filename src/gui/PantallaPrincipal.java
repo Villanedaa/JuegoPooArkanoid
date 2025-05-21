73be145897c80db2c6e9a944db07433b96df53d2
Sebastian <sebastianvillaneda202@gmail.com>
2025-05-21 00:22:40 -0500

    [sebastian_villaneda

--- src/gui/PantallaPrincipal.java
+++ src/gui/PantallaPrincipal.java
@@ -13,16 +13,17 @@ import java.awt.event.KeyEvent;
 import javax.swing.JFrame;
 /**
  *
- * @author Sebastian
+ * @author Sebastian<sebastian.villanedag@autonoma.edu.co>
  */
 
 public class PantallaPrincipal extends JPanel {
-
+   
     private Player jugador;
 
     public PantallaPrincipal() {
         //permite los eventos del teclado
         setFocusable(true);
+        //arregla los parpadeos de pantalla
         setDoubleBuffered(true);
 
         // posicion inicial del jugador
@@ -42,7 +43,14 @@ public class PantallaPrincipal extends JPanel {
             }
         });
 
-        // repintar cada 16 ms
+        
+        /**
+         * 
+         * repintar cada 16 ms
+         * sirve para evitar que el juego o la pantalla se congele
+         * este tambien funciona para que nuestro frame se siga repitando asi no hayan
+         * eventos de teclado
+         */
         Timer timer = new Timer(16, e -> repaint());
         timer.start();
     }
@@ -50,12 +58,15 @@ public class PantallaPrincipal extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        //pintamos el jugador
         jugador.draw(g);
     }
     public static void main(String[] args) {
         JFrame ventanaJuego = new JFrame("ARKANOID-TERROR");
+        //cierra la ventana y finaliza todos los procesos que se esten ejecutando en segundo plano
         ventanaJuego.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ventanaJuego.setSize(800, 600);
+        //centrar la pantalla
         ventanaJuego.setLocationRelativeTo(null);
         ventanaJuego.add(new PantallaPrincipal());
         ventanaJuego.setVisible(true);
