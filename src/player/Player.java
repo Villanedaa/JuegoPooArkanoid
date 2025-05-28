819bcf438025bb21ace1b2aa2dd364a2268f740d
Sebastian <sebastianvillaneda202@gmail.com>
2025-05-28 14:51:10 -0500

    [sebastian_villaneda]: se comento la clase Player

--- src/player/Player.java
+++ src/player/Player.java
@@ -44,61 +44,102 @@ public class Player {
         this.y = y;
         this.width = width;
         this.height = height;
-        this.vidas = 2; // el jugador inicia con 2 vidas
+        //vidas iniciales del jugador
+        this.vidas = 2; 
         imagen = new ImageIcon(getClass().getResource("/images/maderaJugador.jpeg")).getImage();
     }
-
+/*¨*
+    metodo que retorna nuestra coordenada en X
+    */
     public int getX() {
         return x;
     }
+    /**
+     * metodo que asigna un valor a nuestra coordenada en X
+     * @param x : coordenada en X
+     */
 
     public void setX(int x) {
         this.x = x;
     }
-
+    /**
+     * metodo que retorna nuestra coordenada en Y
+     * @return : se retorna y
+     */
     public int getY() {
         return y;
     }
-
+    /**
+     * metodo para asignarle un valor a nuestra coordenada en Y
+     * @param y : coordenada en y
+     */
     public void setY(int y) {
         this.y = y;
     }
-
+    /**
+     * metodo que retorna nuestro ancho
+     * @return : se retorna el ancho
+     */
     public int getWidth() {
         return width;
     }
-
+    /**
+     * metodo para asignarle un valor al ancho
+     * @param width : ancho
+     */
     public void setWidth(int width) {
         this.width = width;
     }
-
+    /**
+     * metodo para obterner nuestro alto
+     * @return : se retorna la variable alto
+     */
     public int getHeight() {
         return height;
     }
-
+    /**
+     * metodo para asignarle un valor a nuestra variable alto
+     * @param height : alto
+     */
     public void setHeight(int height) {
         this.height = height;
     }
-
+    /**
+     * metodo para movernos a la izquierda mediante eventos de teclado
+     */
     public void moverIzquierda() {
         x -= velocidad;
         if (x < 0) x = 0;
     }
-
+    /**
+     * metodo para movernos a la derecha mediante eventos de teclado
+     * @param limitePantalla : se refiere al limite de la pantalla para no salirnos de ella
+     */
     public void moverDerecha(int limitePantalla) {
         x += velocidad;
         if (x + width > limitePantalla) {
             x = limitePantalla - width;
         }
     }
-
+    /**
+     * metodo para divujar nuestro jugador
+     * @param g 
+     */
     public void draw(Graphics g) {
         g.drawImage(imagen, x, y, width, height, null);
     }
-
+    /**
+     * metodo que retorna los limites de la pantalla
+     * @return : limites de pantalla
+     */
     public Rectangle obtenerLimites() {
         return new Rectangle(x, y, width, height);
     }
+    
+    /**
+     * metodo para asignarle un valor a nuestras vidas
+     * @param vidas : vidas del jugador
+     */
     public void setVidas(int vidas){
     
         this.vidas = vidas;
